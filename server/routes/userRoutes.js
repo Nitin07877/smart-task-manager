2c1288d328de00bbebf5ca14ca11afeed3f4b5dd
Nitin07877 <nitinchaudhary12334@gmail.com>
2026-05-01 16:11:59 +0530

    Fix import case for User model in userRoutes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../models/user");
 const authMiddleware = require("../middleware/authMiddleware");
 
 router.get("/", authMiddleware, async (req, res) => {
